7eb3cb3de6f0966d45527243e9088156006eebec
BachDuyHoang <bachduyhoang2k@gmail.com>
2020-06-10 14:47:19 +0700

    new branch

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -17,6 +17,7 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello truong");
+        System.out.println("new branch");
     }
     
 }
